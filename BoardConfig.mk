4cdcd545c4b38fabc9f7bb6d71ce1f24b0fa5bfc
Demon Singur <demonsingur@gmail.com>
2020-01-04 02:55:17 +0530

    vicky: Enable vendor ril services
    Change-Id: I502ab68268ce12834eae3b3e637d9acdd82b9bba
    Signed-off-by: ShivamX087 <shivamx976@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -68,6 +68,9 @@ ZYGOTE_FORCE_64 := true
 # Force any prefer32 targets to be compiled as 64 bit.
 IGNORE_PREFER32_ON_DEVICE := true
 
+# RIL
+ENABLE_VENDOR_RIL_SERVICE := true
+
 # SPL
 BOOT_SECURITY_PATCH := 2025-02-01
 VENDOR_SECURITY_PATCH := 2025-02-01

98688eaba65ecbb6f8ab0da99fe4e9d8030d416a
Aayush Gupta <aayushgupta219@gmail.com>
2023-08-05 12:34:33 +0100

    vicky: Inherit sepolicy for mediatek devices
    There are lot of denials which are not device-specific. Henceforth,
    move such denials to a seperate sepolicy repository and inherit from it.
    
    Signed-off-by: Aayush Gupta <aayushgupta219@gmail.com>
    Change-Id: I95c1f35006d49b797c617c5fea5ff27e5f98d45e
    Signed-off-by: atharvnegi <atharvnegi2951@gmail.com>
    Signed-off-by: ShivamX087 <shivamx976@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -138,6 +138,9 @@ TARGET_USERIMAGES_USE_F2FS := true
 BOOT_SECURITY_PATCH := 2025-02-01
 VENDOR_SECURITY_PATCH := 2025-02-01
 
+# SELinux
+include device/mediatek/sepolicy_vndr/SEPolicy.mk
+
 # Verified Boot
 BOARD_AVB_ENABLE := true
 BOARD_AVB_ALGORITHM := SHA256_RSA4096

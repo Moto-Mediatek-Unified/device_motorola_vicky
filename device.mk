432b6312af9c18a58a81e9a586a32110395d8c33
Sebastiano Barezzi <seba@sebaubuntu.dev>
2023-12-29 02:13:34 +0000

    vicky: Build health AIDL services
    Change-Id: Ibadf22d432791551c16d7f4e386b470574c390f8
    Signed-off-by: ShivamX087 <shivamx976@gmail.com>

--- device.mk
+++ device.mk
@@ -31,6 +31,11 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health-service.mediatek \
+    android.hardware.health-service.mediatek-recovery
+
 # Shipping API level
 PRODUCT_SHIPPING_API_LEVEL := 31
 

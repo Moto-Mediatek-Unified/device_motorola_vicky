b9ff1c66d9a2426a8116e18a83530d0483eabe58
claxten10 <claxten10@gmail.com>
2024-08-31 06:41:20 +0200

    vicky: Define Bootloader and Platform flags
    Signed-off-by: claxten10 <claxten10@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -12,5 +12,14 @@ TARGET_ARCH_VARIANT := armv8-2a-dotprod
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_VARIANT := cortex-a76
 
+# Bootloader
+TARGET_BOOTLOADER_BOARD_NAME := vicky
+TARGET_NO_BOOTLOADER := true
+
+# Platform
+TARGET_BOARD_PLATFORM := mt6789
+BOARD_HAS_MTK_HARDWARE := true
+BOARD_VENDOR := motorola
+
 # Inherit the proprietary files
 include vendor/motorola/vicky/BoardConfigVendor.mk
